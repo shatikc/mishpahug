194309838363a6dfc604e03f5adb479d687c8653
Gregory Sheygam <sheygam@gmail.com>
2018-12-23 18:42:34 +0200

    init project

---
+++ src/main/java/com/telran/mishpahug/MishpahugJava2021Application.java
@@ -0,0 +1,14 @@
+package com.telran.mishpahug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MishpahugJava2021Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MishpahugJava2021Application.class, args);
+    }
+
+}
+
